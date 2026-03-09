4e861b2b61078a56ee140e18f80539c02d30fb35
maria <masikemokid@gmail.com>
2026-03-09 23:35:26 +0300

    AAAAAAAAAA

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  // Добавьте это для Netlify
   trailingSlash: true,
 }
 
